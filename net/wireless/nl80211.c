807650ea5c8d57096c458d39e6fa8d6e30ed3cb8
Vladimir Kondratiev <qca_vkondrat@qca.qualcomm.com>
2012-07-05 14:25:49 +0300

    BACKPORT: {nl,cfg}80211: support high bitrates
    Until now, a u16 value was used to represent bitrate value.
    With VHT bitrates this becomes too small.
    
    Introduce a new 32-bit bitrate attribute. nl80211 will report
    both the new and the old attribute, unless the bitrate doesn't
    fit into the old u16 attribute in which case only the new one
    will be reported.
    
    User space tools encouraged to prefer the 32-bit attribute, if
    available (since it won't be available on older kernels.)
    
    Signed-off-by: Vladimir Kondratiev <qca_vkondrat@qca.qualcomm.com>
    [reword commit message and comments a bit]
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    
    [AdrianDC] Includes if () improvements from commit
               "nl80211/cfg80211: add VHT MCS support"
               by Johannes Berg, Fri, 9 Nov 2012
    
    Change-Id: Ib5823475ba368954e51ac4129b400a58fd74e28d
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- net/wireless/nl80211.c
+++ net/wireless/nl80211.c
@@ -2365,7 +2365,8 @@ static bool nl80211_put_sta_rate(struct sk_buff *msg, struct rate_info *info,
 				 int attr)
 {
 	struct nlattr *rate;
-	u16 bitrate;
+	u32 bitrate;
+	u16 bitrate_compat;
 
 	rate = nla_nest_start(msg, attr);
 	if (!rate)
@@ -2373,8 +2374,14 @@ static bool nl80211_put_sta_rate(struct sk_buff *msg, struct rate_info *info,
 
 	/* cfg80211_calculate_bitrate will return 0 for mcs >= 32 */
 	bitrate = cfg80211_calculate_bitrate(info);
-	if (bitrate > 0)
-		NLA_PUT_U16(msg, NL80211_RATE_INFO_BITRATE, bitrate);
+	/* report 16-bit bitrate only if we can */
+	bitrate_compat = bitrate < (1UL << 16) ? bitrate : 0;
+	if (bitrate > 0 &&
+	    nla_put_u32(msg, NL80211_RATE_INFO_BITRATE32, bitrate))
+		return false;
+	if (bitrate_compat > 0 &&
+	    nla_put_u16(msg, NL80211_RATE_INFO_BITRATE, bitrate_compat))
+		return false;
 
 	if (info->flags & RATE_INFO_FLAGS_MCS)
 		NLA_PUT_U8(msg, NL80211_RATE_INFO_MCS, info->mcs);
