3fc82598e8dc8cd26671f034859b19b4157798c8
Richard W.M. Jones <rjones@redhat.com>
2012-10-09 15:27:43 +0100

    dup3: Return an error when oldfd == newfd.
    Change-Id: I04c4747fb2f7317a9f27fca8d6b994027e6917b7
    Signed-off-by: Richard W.M. Jones <rjones@redhat.com>
    Tested-by: Richard W.M. Jones <rjones@redhat.com>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/file.c
+++ fs/file.c
@@ -890,10 +890,16 @@ SYSCALL_DEFINE3(dup3, unsigned int, oldfd, unsigned int, newfd, int, flags)
 	int err = -EBADF;
 	struct file *file;
 	struct files_struct *files = current->files;
+
 	if ((flags & ~O_CLOEXEC) != 0)
 		return -EINVAL;
+
+	if (unlikely(oldfd == newfd))
+		return -EINVAL;
+
 	if (newfd >= rlimit(RLIMIT_NOFILE))
 		return -EMFILE;
+
 	spin_lock(&files->file_lock);
 	err = expand_files(files, newfd);
 	file = fcheck(oldfd);
