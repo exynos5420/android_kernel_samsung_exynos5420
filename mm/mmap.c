99d347578828d848e3cbdfcec676d1b97b902f91
Oleg Nesterov <oleg@redhat.com>
2013-09-11 14:20:20 -0700

    mm: mmap_region: kill correct_wcount/inode, use allow_write_access()
    correct_wcount and inode in mmap_region() just complicate the code.  This
    boolean was needed previously, when deny_write_access() was called before
    vma_merge(), now we can simply check VM_DENYWRITE and do
    allow_write_access() if it is set.
    
    allow_write_access() checks file != NULL, so this is safe even if it was
    possible to use VM_DENYWRITE && !file.  Just we need to ensure we use the
    same file which was deny_write_access()'ed, so the patch also moves "file
    = vma->vm_file" down after allow_write_access().
    
    Change-Id: I80d3674ce40fb97a80b128ea63edc86ca1770d20
    Signed-off-by: Oleg Nesterov <oleg@redhat.com>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    Cc: Colin Cross <ccross@android.com>
    Cc: David Rientjes <rientjes@google.com>
    Cc: KOSAKI Motohiro <kosaki.motohiro@jp.fujitsu.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/mmap.c
+++ mm/mmap.c
@@ -1285,11 +1285,9 @@ unsigned long mmap_region(struct file *file, unsigned long addr,
 {
 	struct mm_struct *mm = current->mm;
 	struct vm_area_struct *vma, *prev;
-	int correct_wcount = 0;
 	int error;
 	struct rb_node **rb_link, *rb_parent;
 	unsigned long charged = 0;
-	struct inode *inode =  file ? file->f_path.dentry->d_inode : NULL;
 
 	/* Check against address space limit. */
 	if (!may_expand_vm(mm, len >> PAGE_SHIFT)) {
@@ -1377,7 +1375,6 @@ munmap_back:
 			error = deny_write_access(file);
 			if (error)
 				goto free_vma;
-			correct_wcount = 1;
 		}
 		vma->vm_file = file;
 		get_file(file);
@@ -1419,11 +1416,10 @@ munmap_back:
 	}
 
 	vma_link(mm, vma, prev, rb_link, rb_parent);
-	file = vma->vm_file;
-
 	/* Once vma denies write, undo our temporary denial count */
-	if (correct_wcount)
-		atomic_inc(&inode->i_writecount);
+	if (vm_flags & VM_DENYWRITE)
+		allow_write_access(file);
+	file = vma->vm_file;
 out:
 	perf_event_mmap(vma);
 
@@ -1437,8 +1433,8 @@ out:
 	return addr;
 
 unmap_and_free_vma:
-	if (correct_wcount)
-		atomic_inc(&inode->i_writecount);
+	if (vm_flags & VM_DENYWRITE)
+		allow_write_access(file);
 	vma->vm_file = NULL;
 	fput(file);
 
