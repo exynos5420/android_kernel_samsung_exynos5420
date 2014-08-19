a9845f577b875d3d62917581e2fb09e9d4418356
UpInTheAir <tyson_cj@hotmail.com>
2014-08-19 07:55:26 +0700

    exynos_tmu: add configurable interface [halaszk]
    Change-Id: Iaff3ab68ef3c74aa35f8cc2595972579ae7d81d9

--- drivers/thermal/exynos_thermal.c
+++ drivers/thermal/exynos_thermal.c
@@ -125,18 +125,28 @@
 
 #define EXYNOS_GPU_NUMBER	4
 
-#define HOT_NORMAL_TEMP		95
-#define HOT_CRITICAL_TEMP	110
-#define HOT_95			95
-#define HOT_109			104
-#define HOT_110			105
-#define MEM_TH_TEMP1		75
-#define MEM_TH_TEMP2		85
-#define GPU_TH_TEMP1		90
-#define GPU_TH_TEMP2		95
-#define GPU_TH_TEMP3		100
-#define GPU_TH_TEMP4		105
-#define GPU_TH_TEMP5		110
+static unsigned int HOT_NORMAL_TEMP = 95;
+static unsigned int HOT_CRITICAL_TEMP = 110;
+static unsigned int HOT_95 = 95;
+static unsigned int HOT_109 = 109;
+static unsigned int HOT_110 = 110;
+static unsigned int MEM_TH_TEMP1 = 75;
+static unsigned int MEM_TH_TEMP2 = 85;
+static unsigned int GPU_TH_TEMP1 = 90;
+static unsigned int GPU_TH_TEMP2 = 95;
+static unsigned int GPU_TH_TEMP3 = 100;
+static unsigned int GPU_TH_TEMP4 = 105;
+static unsigned int GPU_TH_TEMP5 = 110;
+
+module_param_named(tmu_cpu_normal, HOT_NORMAL_TEMP, uint, S_IWUSR | S_IRUGO);
+module_param_named(tmu_cpu_critical, HOT_CRITICAL_TEMP, uint, S_IWUSR | S_IRUGO);
+module_param_named(tmu_mif_normal, MEM_TH_TEMP1, uint, S_IWUSR | S_IRUGO);
+module_param_named(tmu_mif_hot, MEM_TH_TEMP2, uint, S_IWUSR | S_IRUGO);
+module_param_named(tmu_gpu_temp1, GPU_TH_TEMP1, uint, S_IWUSR | S_IRUGO);
+module_param_named(tmu_gpu_temp2, GPU_TH_TEMP2, uint, S_IWUSR | S_IRUGO);
+module_param_named(tmu_gpu_temp3, GPU_TH_TEMP3, uint, S_IWUSR | S_IRUGO);
+module_param_named(tmu_gpu_temp4, GPU_TH_TEMP4, uint, S_IWUSR | S_IRUGO);
+module_param_named(tmu_gpu_temp5, GPU_TH_TEMP5, uint, S_IWUSR | S_IRUGO);
 
 #define TYPE_CPU		1
 #define TYPE_GPU		2
