6e02b91a01a2554bb8fd575f1694d4ca65497804
Chaithanya Krishna Bacharaju <chaithan@codeaurora.org>
2015-11-07 21:50:22 +0000

    ALSA: compress: Add support to send codec specific data
    Codec specific  metadata is sent only for first stream in gapless
    playback. This causes incorrect configuration to be set for second
    stream and distortions are observed due to framedrops in adsp.
    Add support to send codec specific format during start of
    next stream in gapless using set_next_track_param.
    
    Change-Id: Ieec6b2afedec156e47873efcad9b3571160b0a29
    Signed-off-by: Chaithanya Krishna Bacharaju <chaithan@codeaurora.org>

--- sound/soc/soc-compress.c
+++ sound/soc/soc-compress.c
@@ -334,6 +334,21 @@ static int soc_compr_copy(struct snd_compr_stream *cstream,
 	return ret;
 }
 
+static int sst_compr_set_next_track_param(struct snd_compr_stream *cstream,
+				union snd_codec_options *codec_options)
+{
+	struct snd_soc_pcm_runtime *rtd = cstream->private_data;
+	struct snd_soc_platform *platform = rtd->platform;
+	int ret = 0;
+
+	if (platform->driver->compr_ops &&
+			platform->driver->compr_ops->set_next_track_param)
+		ret = platform->driver->compr_ops->set_next_track_param(cstream,
+								codec_options);
+
+	return ret;
+}
+
 static int sst_compr_set_metadata(struct snd_compr_stream *cstream,
 				struct snd_compr_metadata *metadata)
 {
@@ -365,6 +380,7 @@ static struct snd_compr_ops soc_compr_ops = {
 	.free		= soc_compr_free,
 	.set_params	= soc_compr_set_params,
 	.set_metadata   = sst_compr_set_metadata,
+    .set_next_track_param	= sst_compr_set_next_track_param,
 	.get_metadata	= sst_compr_get_metadata,
 	.get_params	= soc_compr_get_params,
 	.trigger	= soc_compr_trigger,
