be38a37eb101a922572cf9fc43a4cca81d0eb283
dcashman <dcashman@google.com>
2015-12-29 15:07:17 -0800

    FROMLIST: arm: mm: support ARCH_MMAP_RND_BITS.
    (cherry picked from commit https://lkml.org/lkml/2015/12/21/341)
    
    arm: arch_mmap_rnd() uses a hard-code value of 8 to generate the
    random offset for the mmap base address.  This value represents a
    compromise between increased ASLR effectiveness and avoiding
    address-space fragmentation. Replace it with a Kconfig option, which
    is sensibly bounded, so that platform developers may choose where to
    place this compromise. Keep 8 as the minimum acceptable value.
    
    Bug: 24047224
    Signed-off-by: Daniel Cashman <dcashman@android.com>
    Signed-off-by: Daniel Cashman <dcashman@google.com>
    Change-Id: I2f6c18a0060e1c21b53200ecdcfde9a8c2e3db98

--- arch/arm/mm/mmap.c
+++ arch/arm/mm/mmap.c
@@ -259,7 +259,6 @@ void arch_pick_mmap_layout(struct mm_struct *mm)
 {
 	unsigned long random_factor = 0UL;
 
-	/* 8 bits of randomness in 20 address space bits */
 	if ((current->flags & PF_RANDOMIZE) &&
 	    !(current->personality & ADDR_NO_RANDOMIZE))
 		random_factor = (get_random_long() & ((1UL << mmap_rnd_bits) - 1)) << PAGE_SHIFT;
