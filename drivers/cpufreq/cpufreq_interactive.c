bf7ff6b13decdd2ba3dd93d603262135e5884312
Chih-Wei Huang <cwhuang@android-x86.org>
2016-08-30 02:03:35 +0000

    cpufreq: interactive: fix compiling warnings
    The gcc warns like:
    
      cpufreq_interactive.c:745:6: warning: operation on 'ret' may be
    undefined [-Wsequence-point]
    
    It was introduced by commit cf0fad49d17cb8273ce555dd5b7afab67d7923bf.
    
    Since sprintf(...) just return 1 (one character) in this case, ret
    should not changed.
    Just discarding the result of sprintf(...) leads to the result that
    the committer of cf0fad49d17cb8273ce555dd5b7afab67d7923bf wants.
    
    Change-Id: Ifed1cef6d6a31c3ed23dad03a567b3b9eddf3a57
    Signed-off-by: Chih-Wei Huang <cwhuang@android-x86.org>
    Signed-off-by: Dave Daynard <nardholio@gmail.com>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -954,7 +954,7 @@ static ssize_t show_target_loads(
 			       i & 0x1 ? ":" : " ");
 #endif
 
-	ret += sprintf(buf + --ret, "\n");
+	sprintf(buf + ret - 1, "\n");
 	spin_unlock_irqrestore(&target_loads_lock, flags);
 	return ret;
 }
@@ -1022,7 +1022,7 @@ static ssize_t show_above_hispeed_delay(
 			       i & 0x1 ? ":" : " ");
 #endif
 
-	ret += sprintf(buf + --ret, "\n");
+	sprintf(buf + ret - 1, "\n");
 	spin_unlock_irqrestore(&above_hispeed_delay_lock, flags);
 	return ret;
 }
@@ -1447,7 +1447,7 @@ static ssize_t show_cpu_util(
 			ret += sprintf(buf + ret, "OFF ");
 	}
 
-	ret += sprintf(buf + --ret, "\n");
+	sprintf(buf + ret - 1, "\n");
 	return ret;
 }
 
