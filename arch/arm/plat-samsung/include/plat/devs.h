bd085660fa0678046bb3dba3e4d7d4ceb4b257b2
deadman96385 <seanhoyt963@gmail.com>
2016-09-02 01:26:40 -0500

    Remove old gpu drivers
    mali400, orig t6xx, midgard
    
    Change-Id: If92c055eb701856ad8cf0b0b09f9066f51e67855

--- arch/arm/plat-samsung/include/plat/devs.h
+++ arch/arm/plat-samsung/include/plat/devs.h
@@ -192,7 +192,7 @@ extern struct platform_device samsung_device_keypad;
 
 extern struct platform_device s5p_device_fimg2d;
 extern struct platform_device s5p_device_usbswitch;
-#if defined(CONFIG_MALI_T6XX) || defined(CONFIG_PVR_SGX) || defined(CONFIG_MALI_MIDGARD_WK04)
+#if defined(CONFIG_MALI_T6XX) || defined(CONFIG_PVR_SGX) || defined(CONFIG_MALI_MIDGARD_WK04) || defined(CONFIG_MALI_T6XX_R7P0)
 extern struct platform_device exynos5_device_g3d;
 #endif
 extern struct platform_device exynos5410_device_tmu;
