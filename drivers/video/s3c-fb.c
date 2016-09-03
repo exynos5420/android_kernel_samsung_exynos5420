ca30a93430e4da8e6a5d81b4d02f755e45849952
deadman96385 <seanhoyt963@gmail.com>
2016-09-02 22:11:18 -0500

    T805 video driver changes
    Change-Id: Id78e30d56f2048fe9e99c6a0706535ec4a5c3f1d

--- drivers/video/s3c-fb.c
+++ drivers/video/s3c-fb.c
@@ -3489,6 +3489,12 @@ static int s3c_fb_get_user_ion_handle(struct s3c_fb *sfb,
 }
 #endif
 
+static ssize_t s3c_fb_read(struct fb_info *info, char __user *buf,
+		size_t count, loff_t *ppos)
+{
+	return 0;
+}
+
 static int s3c_fb_ioctl(struct fb_info *info, unsigned int cmd,
 			unsigned long arg)
 {
@@ -3712,6 +3718,7 @@ static struct fb_ops s3c_fb_ops = {
 	.fb_fillrect	= cfb_fillrect,
 	.fb_copyarea	= cfb_copyarea,
 	.fb_imageblit	= cfb_imageblit,
+	.fb_read	= s3c_fb_read,
 	.fb_pan_display	= s3c_fb_pan_display,
 	.fb_ioctl	= s3c_fb_ioctl,
 #if !defined(CONFIG_FB_EXYNOS_FIMD_SYSMMU_DISABLE)
