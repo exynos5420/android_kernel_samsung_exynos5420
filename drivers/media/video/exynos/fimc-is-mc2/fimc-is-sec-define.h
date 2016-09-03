3f45ffa8b09eb073f617e04992301821002895a3
Sean Hoyt <seanhoyt963@gmail.com>
2016-09-02 23:39:25 -0500

    Allow using the old hexedited CameraID file location
    Change-Id: I4388e4a37a17593c7fc7e8a6ac0f09e13226374a

--- drivers/media/video/exynos/fimc-is-mc2/fimc-is-sec-define.h
+++ drivers/media/video/exynos/fimc-is-mc2/fimc-is-sec-define.h
@@ -85,12 +85,20 @@
 #define FW_IMX134	"E08LS"
 
 #define SDCARD_FW
+#ifdef CONFIG_CAMERA_HEXEDIT_NEEDED
 #define FIMC_IS_SETFILE_SDCARD_PATH		"/data/camera/"
+#else
+#define FIMC_IS_SETFILE_SDCARD_PATH		"/data/media/0/"
+#endif
 #define FIMC_IS_FW				"fimc_is_fw2.bin"
 #define FIMC_IS_FW_3L2				"fimc_is_fw2_3L2.bin"
 #define FIMC_IS_FW_IMX134			"fimc_is_fw2_IMX134.bin"
 #define FIMC_IS_FW_IMX134_EEPROM		"fimc_is_fw2_IMX134_EEPROM.bin"
+#ifdef CONFIG_CAMERA_HEXEDIT_NEEDED
+#define FIMC_IS_FW_SDCARD			"/data/media/0/fimc_is_fw2.bin"
+#else
 #define FIMC_IS_FW_SDCARD			"/data/camera/fimc_is_fw2.bin"
+#endif
 #define FIMC_IS_IMX135_SETF			"setfile_imx135.bin"
 #define FIMC_IS_IMX134_SETF			"setfile_imx134.bin"
 #define FIMC_IS_IMX134_EEPROM_SETF		"setfile_imx134_eeprom.bin"
@@ -105,7 +113,11 @@
 #define FIMC_IS_SETFILE_VER_OFFSET		0x40
 #define FIMC_IS_SETFILE_VER_SIZE		52
 
+#ifdef CONFIG_CAMERA_HEXEDIT_NEEDED
+#define FIMC_IS_CAL_SDCARD			"/data/camera/cal_data.bin"
+#else
 #define FIMC_IS_CAL_SDCARD			"/data/misc/media/cal_data.bin"
+#endif
 
 #ifdef CONFIG_CAMERA_EEPROM
 #define FIMC_IS_MAX_FW_SIZE			(8 * 1024)
