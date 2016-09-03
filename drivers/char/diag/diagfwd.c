ddf586eed7c409c74c812db45a8bbe48526d303e
deadman96385 <seanhoyt963@gmail.com>
2016-09-02 22:40:28 -0500

    T805 M misc driver updates
    Change-Id: Ie0fd39ac2c279ccb09b1ab17bcba970e42fef75e

--- drivers/char/diag/diagfwd.c
+++ drivers/char/diag/diagfwd.c
@@ -95,7 +95,7 @@ do {									\
 } while (0)
 
 #define CHK_OVERFLOW(bufStart, start, end, length) \
-((bufStart <= start) && (end - start >= length)) ? 1 : 0
+	((bufStart <= start) && (end - start >= length) && (length > 0)) ? 1 : 0
 
 /* Determine if this device uses a device tree */
 #ifdef CONFIG_OF
@@ -1447,6 +1447,12 @@ void diag_process_hdlc(void *data, unsigned len)
 	hdlc.escaping = 0;
 
 	ret = diag_hdlc_decode(&hdlc);
+
+	if (hdlc.dest_idx < 4) {
+		pr_err_ratelimited("diag: In %s, message is too short, len: %d,"
+			" dest len: %d\n", __func__, len, hdlc.dest_idx);
+		return;
+	}
 
 	if (ret)
 		type = diag_process_apps_pkt(driver->hdlc_buf,
