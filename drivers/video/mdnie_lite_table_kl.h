77fc0b60307b747e434393481a44b03efa82c6ae
deadman96385 <seanhoyt963@gmail.com>
2016-09-02 22:11:18 -0500

    T805 M video driver changes
    Change-Id: Id78e30d56f2048fe9e99c6a0706535ec4a5c3f1d

--- drivers/video/mdnie_lite_table_kl.h
+++ drivers/video/mdnie_lite_table_kl.h
@@ -1,7 +1,7 @@
 #ifndef __MDNIE_TABLE_H__
 #define __MDNIE_TABLE_H__
 
-/* 2015.03.03 */
+/* 2016.04.19 */
 /* SCR Position can be different each panel */
 #define ASCR_CMD		MDNIE_CMD1
 
@@ -40,6 +40,322 @@ static unsigned char coordinate_data[][3] = {
 	{0xfd, 0xff, 0xfa}, /* Tune_7 */
 	{0xfc, 0xff, 0xfc}, /* Tune_8 */
 	{0xfb, 0xff, 0xff}, /* Tune_9 */
+};
+
+static unsigned char GRAYSCALE_1[] = {
+	/* start */
+	0xEB,
+	0x01, /* mdnie_en */
+	0x00, /* data_width mask 00 0000 */
+	0x03, /* ascr_roi 1 ascr 00 1 0 */
+	0x02, /* algo_roi 1 algo lce_roi 1 lce 00 1 0 00 1 0 */
+	0x00, /* roi_ctrl 00 */
+	0x00, /* roi0_x_start 12 */
+	0x00,
+	0x00, /* roi0_x_end */
+	0x00,
+	0x00, /* roi0_y_start */
+	0x00,
+	0x00, /* roi0_y_end */
+	0x00,
+	0x00, /* roi1_x_strat */
+	0x00,
+	0x00, /* roi1_x_end */
+	0x00,
+	0x00, /* roi1_y_start */
+	0x00,
+	0x00, /* roi1_y_end */
+	0x00,
+};
+
+static unsigned char GRAYSCALE_2[] = {
+	0xEC,
+	0x18, /* lce_gain 00 0000 */
+	0x24, /* lce_color_gain 00 0000 */
+	0x10, /* lce_scene_change_on scene_trans 0 0000 */
+	0x14, /* lce_min_diff */
+	0xb3, /* lce_illum_gain */
+	0x01, /* lce_ref_offset 9 */
+	0x0e,
+	0x01, /* lce_ref_gain 9 */
+	0x00,
+	0x66, /* lce_block_size h v 0000 0000 */
+	0xfa, /* lce_bright_th */
+	0x2d, /* lce_bin_size_ratio */
+	0x03, /* lce_dark_th 000 */
+	0x96, /* lce_min_ref_offset */
+	0x00, /* nr sharp cs gamma 0000 */
+	0xff, /* nr_mask_th */
+	0x00, /* sharpen_weight 10 */
+	0x00,
+	0x07, /* sharpen_maxplus 11 */
+	0xff,
+	0x07, /* sharpen_maxminus 11 */
+	0xff,
+	0x01, /* cs_gain 10 */
+	0x00,
+	0x00, /* curve_1_b */
+	0x20, /* curve_1_a */
+	0x00, /* curve_2_b */
+	0x20, /* curve_2_a */
+	0x00, /* curve_3_b */
+	0x20, /* curve_3_a */
+	0x00, /* curve_4_b */
+	0x20, /* curve_4_a */
+	0x02, /* curve_5_b */
+	0x1b, /* curve_5_a */
+	0x02, /* curve_6_b */
+	0x1b, /* curve_6_a */
+	0x02, /* curve_7_b */
+	0x1b, /* curve_7_a */
+	0x02, /* curve_8_b */
+	0x1b, /* curve_8_a */
+	0x09, /* curve_9_b */
+	0xa6, /* curve_9_a */
+	0x09, /* curve10_b */
+	0xa6, /* curve10_a */
+	0x09, /* curve11_b */
+	0xa6, /* curve11_a */
+	0x09, /* curve12_b */
+	0xa6, /* curve12_a */
+	0x00, /* curve13_b */
+	0x20, /* curve13_a */
+	0x00, /* curve14_b */
+	0x20, /* curve14_a */
+	0x00, /* curve15_b */
+	0x20, /* curve15_a */
+	0x00, /* curve16_b */
+	0x20, /* curve16_a */
+	0x00, /* curve17_b */
+	0x20, /* curve17_a */
+	0x00, /* curve18_b */
+	0x20, /* curve18_a */
+	0x00, /* curve19_b */
+	0x20, /* curve19_a */
+	0x00, /* curve20_b */
+	0x20, /* curve20_a */
+	0x00, /* curve21_b */
+	0x20, /* curve21_a */
+	0x00, /* curve22_b */
+	0x20, /* curve22_a */
+	0x00, /* curve23_b */
+	0x20, /* curve23_a */
+	0x00, /* curve24_b */
+	0xFF, /* curve24_a */
+	0x20, /* ascr_skin_on strength 0 00000 */
+	0x67, /* ascr_skin_cb */
+	0xa9, /* ascr_skin_cr */
+	0x0c, /* ascr_dist_up */
+	0x0c, /* ascr_dist_down */
+	0x0c, /* ascr_dist_right */
+	0x0c, /* ascr_dist_left */
+	0x00, /* ascr_div_up 20 */
+	0xaa,
+	0xab,
+	0x00, /* ascr_div_down */
+	0xaa,
+	0xab,
+	0x00, /* ascr_div_right */
+	0xaa,
+	0xab,
+	0x00, /* ascr_div_left */
+	0xaa,
+	0xab,
+	0xff, /* ascr_skin_Rr */
+	0x00, /* ascr_skin_Rg */
+	0x00, /* ascr_skin_Rb */
+	0xff, /* ascr_skin_Yr */
+	0xff, /* ascr_skin_Yg */
+	0x00, /* ascr_skin_Yb */
+	0xff, /* ascr_skin_Mr */
+	0x00, /* ascr_skin_Mg */
+	0xff, /* ascr_skin_Mb */
+	0xff, /* ascr_skin_Wr */
+	0xff, /* ascr_skin_Wg */
+	0xff, /* ascr_skin_Wb */
+	0xb3, /* ascr_Cr */
+	0x4c, /* ascr_Rr */
+	0xb3, /* ascr_Cg */
+	0x4c, /* ascr_Rg */
+	0xb3, /* ascr_Cb */
+	0x4c, /* ascr_Rb */
+	0x69, /* ascr_Mr */
+	0x96, /* ascr_Gr */
+	0x69, /* ascr_Mg */
+	0x96, /* ascr_Gg */
+	0x69, /* ascr_Mb */
+	0x96, /* ascr_Gb */
+	0xe2, /* ascr_Yr */
+	0x1d, /* ascr_Br */
+	0xe2, /* ascr_Yg */
+	0x1d, /* ascr_Bg */
+	0xe2, /* ascr_Yb */
+	0x1d, /* ascr_Bb */
+	0xff, /* ascr_Wr */
+	0x00, /* ascr_Kr */
+	0xff, /* ascr_Wg */
+	0x00, /* ascr_Kg */
+	0xff, /* ascr_Wb */
+	0x00, /* ascr_Kb */
+	/* end */
+};
+
+static unsigned char GRAYSCALE_NEGATIVE_1[] = {
+	/* start */
+	0xEB,
+	0x01, /* mdnie_en */
+	0x00, /* data_width mask 00 0000 */
+	0x03, /* ascr_roi 1 ascr 00 1 0 */
+	0x02, /* algo_roi 1 algo lce_roi 1 lce 00 1 0 00 1 0 */
+	0x00, /* roi_ctrl 00 */
+	0x00, /* roi0_x_start 12 */
+	0x00,
+	0x00, /* roi0_x_end */
+	0x00,
+	0x00, /* roi0_y_start */
+	0x00,
+	0x00, /* roi0_y_end */
+	0x00,
+	0x00, /* roi1_x_strat */
+	0x00,
+	0x00, /* roi1_x_end */
+	0x00,
+	0x00, /* roi1_y_start */
+	0x00,
+	0x00, /* roi1_y_end */
+	0x00,
+};
+
+static unsigned char GRAYSCALE_NEGATIVE_2[] = {
+	0xEC,
+	0x18, /* lce_gain 00 0000 */
+	0x24, /* lce_color_gain 00 0000 */
+	0x10, /* lce_scene_change_on scene_trans 0 0000 */
+	0x14, /* lce_min_diff */
+	0xb3, /* lce_illum_gain */
+	0x01, /* lce_ref_offset 9 */
+	0x0e,
+	0x01, /* lce_ref_gain 9 */
+	0x00,
+	0x66, /* lce_block_size h v 0000 0000 */
+	0xfa, /* lce_bright_th */
+	0x2d, /* lce_bin_size_ratio */
+	0x03, /* lce_dark_th 000 */
+	0x96, /* lce_min_ref_offset */
+	0x00, /* nr sharp cs gamma 0000 */
+	0xff, /* nr_mask_th */
+	0x00, /* sharpen_weight 10 */
+	0x00,
+	0x07, /* sharpen_maxplus 11 */
+	0xff,
+	0x07, /* sharpen_maxminus 11 */
+	0xff,
+	0x01, /* cs_gain 10 */
+	0x00,
+	0x00, /* curve_1_b */
+	0x20, /* curve_1_a */
+	0x00, /* curve_2_b */
+	0x20, /* curve_2_a */
+	0x00, /* curve_3_b */
+	0x20, /* curve_3_a */
+	0x00, /* curve_4_b */
+	0x20, /* curve_4_a */
+	0x02, /* curve_5_b */
+	0x1b, /* curve_5_a */
+	0x02, /* curve_6_b */
+	0x1b, /* curve_6_a */
+	0x02, /* curve_7_b */
+	0x1b, /* curve_7_a */
+	0x02, /* curve_8_b */
+	0x1b, /* curve_8_a */
+	0x09, /* curve_9_b */
+	0xa6, /* curve_9_a */
+	0x09, /* curve10_b */
+	0xa6, /* curve10_a */
+	0x09, /* curve11_b */
+	0xa6, /* curve11_a */
+	0x09, /* curve12_b */
+	0xa6, /* curve12_a */
+	0x00, /* curve13_b */
+	0x20, /* curve13_a */
+	0x00, /* curve14_b */
+	0x20, /* curve14_a */
+	0x00, /* curve15_b */
+	0x20, /* curve15_a */
+	0x00, /* curve16_b */
+	0x20, /* curve16_a */
+	0x00, /* curve17_b */
+	0x20, /* curve17_a */
+	0x00, /* curve18_b */
+	0x20, /* curve18_a */
+	0x00, /* curve19_b */
+	0x20, /* curve19_a */
+	0x00, /* curve20_b */
+	0x20, /* curve20_a */
+	0x00, /* curve21_b */
+	0x20, /* curve21_a */
+	0x00, /* curve22_b */
+	0x20, /* curve22_a */
+	0x00, /* curve23_b */
+	0x20, /* curve23_a */
+	0x00, /* curve24_b */
+	0xFF, /* curve24_a */
+	0x20, /* ascr_skin_on strength 0 00000 */
+	0x67, /* ascr_skin_cb */
+	0xa9, /* ascr_skin_cr */
+	0x0c, /* ascr_dist_up */
+	0x0c, /* ascr_dist_down */
+	0x0c, /* ascr_dist_right */
+	0x0c, /* ascr_dist_left */
+	0x00, /* ascr_div_up 20 */
+	0xaa,
+	0xab,
+	0x00, /* ascr_div_down */
+	0xaa,
+	0xab,
+	0x00, /* ascr_div_right */
+	0xaa,
+	0xab,
+	0x00, /* ascr_div_left */
+	0xaa,
+	0xab,
+	0xff, /* ascr_skin_Rr */
+	0x00, /* ascr_skin_Rg */
+	0x00, /* ascr_skin_Rb */
+	0xff, /* ascr_skin_Yr */
+	0xff, /* ascr_skin_Yg */
+	0x00, /* ascr_skin_Yb */
+	0xff, /* ascr_skin_Mr */
+	0x00, /* ascr_skin_Mg */
+	0xff, /* ascr_skin_Mb */
+	0xff, /* ascr_skin_Wr */
+	0xff, /* ascr_skin_Wg */
+	0xff, /* ascr_skin_Wb */
+	0x4c, /* ascr_Cr */
+	0xb3, /* ascr_Rr */
+	0x4c, /* ascr_Cg */
+	0xb3, /* ascr_Rg */
+	0x4c, /* ascr_Cb */
+	0xb3, /* ascr_Rb */
+	0x96, /* ascr_Mr */
+	0x69, /* ascr_Gr */
+	0x96, /* ascr_Mg */
+	0x69, /* ascr_Gg */
+	0x96, /* ascr_Mb */
+	0x69, /* ascr_Gb */
+	0x1d, /* ascr_Yr */
+	0xe2, /* ascr_Br */
+	0x1d, /* ascr_Yg */
+	0xe2, /* ascr_Bg */
+	0x1d, /* ascr_Yb */
+	0xe2, /* ascr_Bb */
+	0x00, /* ascr_Wr */
+	0xff, /* ascr_Kr */
+	0x00, /* ascr_Wg */
+	0xff, /* ascr_Kg */
+	0x00, /* ascr_Wb */
+	0xff, /* ascr_Kb */
+	/* end */
 };
 
 ////////////////// UI /// /////////////////////
@@ -7174,7 +7490,9 @@ struct mdnie_table bypass_table[BYPASS_MAX] = {
 struct mdnie_table accessibility_table[ACCESSIBILITY_MAX] = {
 	[NEGATIVE] = MDNIE_SET(NEGATIVE),
 	MDNIE_SET(COLOR_BLIND),
-	MDNIE_SET(SCREEN_CURTAIN)
+	MDNIE_SET(SCREEN_CURTAIN),
+	MDNIE_SET(GRAYSCALE),
+	MDNIE_SET(GRAYSCALE_NEGATIVE)
 };
 
 struct mdnie_table hbm_table[HBM_MAX] = {
