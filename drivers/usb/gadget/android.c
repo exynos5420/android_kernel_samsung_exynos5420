98870e767ad0a8a30f9ff23c74c86e3b2e877864
Sean Hoyt <seanhoyt963@gmail.com>
2016-09-22 22:04:10 -0500

    Revert "Compile stock and Samsung MTP together [AndreiLux]"
    This reverts commit d7c9519a90849000109279674fa8dc37bafcd507.
    
    Change-Id: Ic741dbfe03ab1ffb83f470771dee51c22cc5b28e

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -23,7 +23,6 @@
 #include <linux/kernel.h>
 #include <linux/utsname.h>
 #include <linux/platform_device.h>
-#include <linux/runtime_dependency.h>
 
 #include <linux/usb/ch9.h>
 #include <linux/usb/composite.h>
@@ -52,8 +51,11 @@
 #endif
 #include "f_acm.c"
 #include "f_adb.c"
-#include "f_mtp.c"
+#ifdef CONFIG_USB_ANDROID_SAMSUNG_MTP
 #include "f_mtp_samsung.c"
+#else
+#include "f_mtp.c"
+#endif
 
 #ifdef CONFIG_USB_ANDROID_SAMSUNG_SIDESYNC
 #include "f_conn_gadget.c"
@@ -639,29 +641,20 @@ static struct android_usb_function acm_function = {
 static int
 mtp_function_init(struct android_usb_function *f,
 		struct usb_composite_dev *cdev)
-{	
-	if (rt_is_flag(MTP_SAMSUNG))
-		return _mtp_setup();
-	else
-		return mtp_setup();
+{
+	return mtp_setup();
 }
 
 static void mtp_function_cleanup(struct android_usb_function *f)
 {
-	if (rt_is_flag(MTP_SAMSUNG))
-		_mtp_cleanup();
-	else
-		mtp_cleanup();
+	mtp_cleanup();
 }
 
 static int
 mtp_function_bind_config(struct android_usb_function *f,
 		struct usb_configuration *c)
 {
-	if (rt_is_flag(MTP_SAMSUNG))
-		return _mtp_bind_config(c, false);
-	else
-		return mtp_bind_config(c, false);
+	return mtp_bind_config(c, false);
 }
 
 static int
@@ -681,20 +674,14 @@ static int
 ptp_function_bind_config(struct android_usb_function *f,
 		struct usb_configuration *c)
 {
-	if (rt_is_flag(MTP_SAMSUNG))
-		return _mtp_bind_config(c, true);
-	else
-		return mtp_bind_config(c, true);
+	return mtp_bind_config(c, true);
 }
 
 static int mtp_function_ctrlrequest(struct android_usb_function *f,
 					struct usb_composite_dev *cdev,
 					const struct usb_ctrlrequest *c)
 {
-	if (rt_is_flag(MTP_SAMSUNG))
-		return _mtp_ctrlrequest(cdev, c);
-	else
-		return mtp_ctrlrequest(cdev, c);
+	return mtp_ctrlrequest(cdev, c);
 }
 
 static struct android_usb_function mtp_function = {
