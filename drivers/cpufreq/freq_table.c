126e583c0ae1745db2eb2c2ac05615d943cbcb33
tincho5588 <tincho5588@gmail.com>
2016-10-07 00:22:04 -0300

    Move diff declaration in cpufreq table
    Change-Id: I28d532814770687a08445b44ce397d6a1e0f1f19

--- drivers/cpufreq/freq_table.c
+++ drivers/cpufreq/freq_table.c
@@ -23,7 +23,7 @@ int cpufreq_frequency_table_cpuinfo(struct cpufreq_policy *policy,
 {
 	unsigned int min_freq = ~0;
 	unsigned int max_freq = 0;
-	unsigned int i, diff;
+	unsigned int i;
 
 	for (i = 0; (table[i].frequency != CPUFREQ_TABLE_END); i++) {
 		unsigned int freq = table[i].frequency;
@@ -109,7 +109,7 @@ int cpufreq_frequency_table_target(struct cpufreq_policy *policy,
 		.index = ~0,
 		.frequency = 0,
 	};
-	unsigned int i;
+	unsigned int i, diff;
 
 	pr_debug("request for target %u kHz (relation: %u) for cpu %u\n",
 					target_freq, relation, policy->cpu);
