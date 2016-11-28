54067f4eb46231d8fd6040824da719f7d873e0f4
zakooz <marcotribosklol@gmail.com>
2016-11-28 19:46:34 +0000

    Revert "gpu: arm: r7p0: Fix compilation without CONFIG_MALI_EXYNOS_TRACE"
    This reverts commit 36e4dd268155840ac8b7bdd407b05d66e0793b1f.

--- drivers/gpu/arm/t6xx/r7p0/mali_kbase_core_linux.c
+++ drivers/gpu/arm/t6xx/r7p0/mali_kbase_core_linux.c
@@ -3222,11 +3222,9 @@ static int kbase_device_debugfs_init(struct kbase_device *kbdev)
 	kbdev->debugfs_ctx_directory = debugfs_create_dir("ctx",
 			kbdev->mali_debugfs_directory);
 #else
-#ifdef CONFIG_MALI_EXYNOS_TRACE
 	kbdev->trace_dentry = debugfs_create_file("mali_trace", S_IRUGO,
 			kbdev->mali_debugfs_directory, kbdev,
 			&kbasep_trace_debugfs_fops);
-#endif
 
 	kbdev->debugfs_ctx_directory = debugfs_create_dir("mem",
 			kbdev->mali_debugfs_directory);
