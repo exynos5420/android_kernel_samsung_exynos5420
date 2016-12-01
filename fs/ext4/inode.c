5a2bcf263f19c765b08f0c915f4e926ba2581e0a
Eric Biggers <ebiggers@google.com>
2016-12-01 14:51:58 -0500

    ext4: don't read out of bounds when checking for in-inode xattrs
    commit 290ab230016f187c3551d8380ea742889276d03a upstream.
    
    With i_extra_isize equal to or close to the available space, it was
    possible for us to read past the end of the inode when trying to detect
    or validate in-inode xattrs.  Fix this by checking for the needed extra
    space first.
    
    This patch shouldn't have any noticeable effect on
    non-corrupted/non-malicious filesystems.
    
    Change-Id: Ib028035886a88eb32e2678264c7390fc8bac6900
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Reviewed-by: Andreas Dilger <adilger@dilger.ca>
    [bwh: Backported to 3.16: adjust context]
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>

--- fs/ext4/inode.c
+++ fs/ext4/inode.c
@@ -3828,7 +3828,9 @@ struct inode *ext4_iget(struct super_block *sb, unsigned long ino)
 			__le32 *magic = (void *)raw_inode +
 					EXT4_GOOD_OLD_INODE_SIZE +
 					ei->i_extra_isize;
-			if (*magic == cpu_to_le32(EXT4_XATTR_MAGIC))
+			if (EXT4_GOOD_OLD_INODE_SIZE + ei->i_extra_isize +
+				sizeof(__le32) <= EXT4_INODE_SIZE(inode->i_sb) &&
+			    *magic == cpu_to_le32(EXT4_XATTR_MAGIC))
 				ext4_set_inode_state(inode, EXT4_STATE_XATTR);
 		}
 	} else
