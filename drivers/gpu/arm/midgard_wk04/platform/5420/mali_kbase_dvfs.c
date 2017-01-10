e595e211680f2248cacd9155b7fb58ef8040a2a2
tincho5588 <tincho5588@gmail.com>
2017-01-10 06:17:54 -0300

    GPU: custom interface fixes
    Fixed a derp in mali_kbase_dvfs.c that locked the clocks at 177mhz for min and 667mhz for max.
    The max and min clocks nodes now report real values.
    The dvfs now changes when u set a new max/min clock to reflect the changes.
    
    Change-Id: I52927190c83fc38856b3dfd9180e13ea45a5e6a1

--- drivers/gpu/arm/midgard_wk04/platform/5420/mali_kbase_dvfs.c
+++ drivers/gpu/arm/midgard_wk04/platform/5420/mali_kbase_dvfs.c
@@ -282,9 +282,19 @@ void hlpr_set_min_max_G3D(unsigned int min, unsigned int max)
 			dvfs_step_max_minus1 = mali_dvfs_infotbl[i-1].clock;
 		}
 	}
-	
+
 	hlpr_set_gpu_gov_table(gov_table[dvfs_step_max - dvfs_step_min - 1]);
-	
+
+}
+
+unsigned int hlpr_get_min_clock(void)
+{
+	return 	mali_dvfs_infotbl[dvfs_step_min].clock;
+}
+
+unsigned int hlpr_get_max_clock(void)
+{
+	return 	mali_dvfs_infotbl[dvfs_step_max-1].clock;
 }
 
 static int mali_dvfs_update_asv(int cmd)
@@ -375,8 +385,6 @@ skip:
 	}
 #endif
 	spin_unlock_irqrestore(&mali_dvfs_spinlock, flags);
-	
-	hlpr_set_min_max_G3D(MALI_DVFS_START_FREQ, MALI_DVFS_BL_CONFIG_FREQ);
 
 }
 
@@ -601,6 +609,8 @@ int kbase_platform_dvfs_init(struct kbase_device *kbdev)
 	mali_dvfs_status_current.asv_status = ASV_STATUS_NOT_INIT;
 #endif
 	spin_unlock_irqrestore(&mali_dvfs_spinlock, flags);
+
+	hlpr_set_min_max_G3D(MALI_DVFS_START_FREQ, MALI_DVFS_BL_CONFIG_FREQ);
 
 	return MALI_TRUE;
 }
@@ -1311,7 +1321,7 @@ int mali_get_dvfs_table(char *buf, size_t buf_size)
 	if (buf == NULL)
 		return 0;
 
-	for (i = MALI_DVFS_STEP-1; i >= 0; i--)
+	for (i = dvfs_step_max-1; i >= dvfs_step_min; i--)
 		cnt += snprintf(buf+cnt, buf_size-cnt, " %d", mali_dvfs_infotbl[i].clock);
 	return cnt;
 }
