72e2e300ea903b7101b1dcc6a53a9bbd3cd5fa15
Christopher N. Hesse <raymanfx@gmail.com>
2017-01-27 19:56:49 +0100

    video: mdnie: Lift RGB tuning restrictions
    Change-Id: Ib9a5c6e25143559ca0ad8b9671c9534ca4ff26ca
    (cherry picked from commit e400ee1ed56db8aa46eddb57fa941f2b91ec9592)

--- drivers/video/mdnie_lite.c
+++ drivers/video/mdnie_lite.c
@@ -647,10 +647,7 @@ static ssize_t sensorRGB_store(struct device *dev,
 	if (ret < 0)
 		return ret;
 
-	if (mdnie->enable && (mdnie->accessibility == ACCESSIBILITY_OFF)
-		&& (mdnie->mode == AUTO)
-		&& ((mdnie->scenario == BROWSER_MODE)
-		|| (mdnie->scenario == EBOOK_MODE))) {
+	if (mdnie->enable) {
 		dev_info(dev, "%s, white_r %d, white_g %d, white_b %d\n",
 			__func__, white_red, white_green, white_blue);
 
