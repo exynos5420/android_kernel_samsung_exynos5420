0406f70f49a849ecf222e96c12647786e4712f2a
ShevT <shev_t@mail.ru>
2017-02-03 15:50:03 +0300

    sec_log: Do not create /proc/last_kmsg second time
    /proc/last_kmsg created in drivers/staging/android/ram_console.c:
    ...
    entry = create_proc_entry("last_kmsg", S_IFREG | S_IRUGO, NULL);
    ...
    
    Re-create - generates a warning:
    ...
    [    3.552610]  [B0:       swapper/0:     1] ------------[ cut here ]------------
    [    3.552685]  [B0:       swapper/0:     1] WARNING: at ../../../../../../kernel/samsung/lt033g/fs/proc/generic.c:587 proc_register+0xac/0x144()
    [    3.552776]  [B0:       swapper/0:     1] proc_dir_entry '/proc/last_kmsg' already registered
    [    3.552840]  [B0:       swapper/0:     1] Backtrace:
    [    3.552900]  [B0:       swapper/0:     1] [<c0012cd4>] (dump_backtrace+0x0/0x10c) from [<c080dd18>] (dump_stack+0x18/0x1c)
    [    3.552980]  [B0:       swapper/0:     1]  r7:0000024b r6:c0a017b0 r5:00000009 r4:de83def0
    [    3.553072]  [B0:       swapper/0:     1] [<c080dd00>] (dump_stack+0x0/0x1c) from [<c00435a0>] (warn_slowpath_common+0x58/0x68)
    [    3.553165]  [B0:       swapper/0:     1] [<c0043548>] (warn_slowpath_common+0x0/0x68) from [<c004361c>] (warn_slowpath_fmt+0x38/0x40)
    [    3.553251]  [B0:       swapper/0:     1]  r9:00000007 r8:ddffef80 r7:c0c60180 r6:dd9a8180 r5:dd9a81d5
    [    3.553426]  [B0:       swapper/0:     1] r4:c0a017ec
    [    3.553485]  [B0:       swapper/0:     1] [<c00435e4>] (warn_slowpath_fmt+0x0/0x40) from [<c0162de0>] (proc_register+0xac/0x144)
    [    3.553568]  [B0:       swapper/0:     1]  r3:c0c601d5 r2:c0a017ec
    [    3.553627]  [B0:       swapper/0:     1]  r4:ddffee80
    [    3.553687]  [B0:       swapper/0:     1] [<c0162d34>] (proc_register+0x0/0x144) from [<c01630bc>] (create_proc_entry+0x5c/0x9c)
    [    3.553769]  [B0:       swapper/0:     1]  r8:c0b29270 r7:de83c008 r6:c1205360 r5:dea3a580 r4:dd9a8180
    [    3.553897]  [B0:       swapper/0:     1] [<c0163060>] (create_proc_entry+0x0/0x9c) from [<c0b17094>] (ram_console_late_init+0x44/0xac)
    [    3.553981]  [B0:       swapper/0:     1]  r4:c0cca400
    [    3.554038]  [B0:       swapper/0:     1] [<c0b17050>] (ram_console_late_init+0x0/0xac) from [<c0aeb9b4>] (do_one_initcall+0xa8/0x164)
    [    3.554122]  [B0:       swapper/0:     1]  r7:de83c008 r6:c0b17050 r5:c0cca400 r4:c0cca400
    [    3.554209]  [B0:       swapper/0:     1] [<c0aeb90c>] (do_one_initcall+0x0/0x164) from [<c0aebbb8>] (kernel_init+0x148/0x1e4)
    [    3.554298]  [B0:       swapper/0:     1] [<c0aeba70>] (kernel_init+0x0/0x1e4) from [<c0047a28>] (do_exit+0x0/0x7c4)
    [    3.554374]  [B0:       swapper/0:     1]  r7:00000013
    [    3.554422]  [B0:       swapper/0:     1] ---[ end trace 1ae670901263767c ]---
    ...
    
    Change-Id: I4b912e7ac09ad07243c6b369e9f9f4bc30e5ba4b

--- arch/arm/mach-exynos/sec_log.c
+++ arch/arm/mach-exynos/sec_log.c
@@ -581,26 +581,6 @@ static const struct file_operations last_kmsg_file_ops = {
 	.owner = THIS_MODULE,
 	.read = sec_log_read_old,
 };
-
-static int __init sec_log_last_kmsg_late_init(void)
-{
-	struct proc_dir_entry *entry;
-
-	if (last_kmsg_buffer == NULL)
-		return 0;
-
-	entry = create_proc_entry("last_kmsg", S_IFREG | S_IRUGO, NULL);
-	if (!entry) {
-		pr_err("%s: failed to create proc entry\n", __func__);
-		return 0;
-	}
-
-	entry->proc_fops = &last_kmsg_file_ops;
-	entry->size = last_kmsg_size;
-	return 0;
-}
-
-late_initcall(sec_log_last_kmsg_late_init);
 #endif
 
 #ifdef CONFIG_SEC_DEBUG_TIMA_LOG
