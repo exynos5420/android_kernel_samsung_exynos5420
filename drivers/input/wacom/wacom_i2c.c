cf60347cb269366f87c471bc9bf29fe97c9ea7ae
Thomas Patry <thompatry@gmail.com>
2017-12-05 20:34:41 -0600

    wacom: Report KEY_WAKEUP on s-pen removal
    * Wakes device when s-pen is removed
    
    Change-Id: I4ccf70ded6449ca0719b9991d16a69e6c0e4f2b2

--- drivers/input/wacom/wacom_i2c.c
+++ drivers/input/wacom/wacom_i2c.c
@@ -214,9 +214,12 @@ static void pen_insert_work(struct work_struct *work)
 	printk(KERN_DEBUG "epen:%s : %d\n",
 		__func__, wac_i2c->pen_insert);
 
-	input_report_switch(wac_i2c->input_dev,
-		SW_PEN_INSERT, !wac_i2c->pen_insert);
-	input_sync(wac_i2c->input_dev);
+	if (!wac_i2c->pen_insert) {
+		input_report_key(wac_i2c->input_dev, KEY_WAKEUP, 1);
+		input_sync(wac_i2c->input_dev);
+		input_report_key(wac_i2c->input_dev, KEY_WAKEUP, 0);
+		input_sync(wac_i2c->input_dev);
+	}
 
 #ifdef BATTERY_SAVING_MODE
 	if (wac_i2c->pen_insert) {
@@ -309,9 +312,6 @@ static void wacom_i2c_set_input_values(struct i2c_client *client,
 	input_dev->id.bustype = BUS_I2C;
 	input_dev->dev.parent = &client->dev;
 	input_dev->evbit[0] |= BIT_MASK(EV_KEY) | BIT_MASK(EV_ABS);
-
-	input_dev->evbit[0] |= BIT_MASK(EV_SW);
-	input_set_capability(input_dev, EV_SW, SW_PEN_INSERT);
 
 	input_dev->open = wacom_i2c_input_open;
 	input_dev->close = wacom_i2c_input_close;
