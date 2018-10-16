0df42abfbd724bcb1cf9eb6120146fc8a45191fc
Kyeongdon Kim <kyeongdon.kim@lge.com>
2018-10-16 14:57:26 +0900

    net: fix warning in af_unix
    [ Upstream commit 33c4368ee2589c165aebd8d388cbd91e9adb9688 ]
    
    This fixes the "'hash' may be used uninitialized in this function"
    
    net/unix/af_unix.c:1041:20: warning: 'hash' may be used uninitialized in this function [-Wmaybe-uninitialized]
      addr->hash = hash ^ sk->sk_type;
    
    Signed-off-by: Kyeongdon Kim <kyeongdon.kim@lge.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Change-Id: Ie03c959f1d3d4521bf27ba960eca098685e6ccc2

--- net/unix/af_unix.c
+++ net/unix/af_unix.c
@@ -202,6 +202,8 @@ static inline void unix_release_addr(struct unix_address *addr)
 
 static int unix_mkname(struct sockaddr_un *sunaddr, int len, unsigned *hashp)
 {
+	*hashp = 0;
+
 	if (len <= sizeof(short) || len > sizeof(*sunaddr))
 		return -EINVAL;
 	if (!sunaddr || sunaddr->sun_family != AF_UNIX)
