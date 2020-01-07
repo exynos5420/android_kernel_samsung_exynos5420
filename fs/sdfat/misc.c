053893b929bfaf3f68f91172a3acfc94393656f6
Vasyl Gello <vasek.gello@gmail.com>
2020-01-07 19:12:45 +0000

    Revert "fs: sdfat: Fix compilation on Linux >= 4.16"
    This reverts commit 935483f871cc04aa0e36d5d2cb7940451964aa99.
    
    Change-Id: Id946da93973c57c4aa6bdc08e1b254d917f22944

--- fs/sdfat/misc.c
+++ fs/sdfat/misc.c
@@ -52,6 +52,14 @@
 #define ST_LOG(fmt, ...)
 #endif
 
+/*************************************************************************
+ * FUNCTIONS WHICH HAS KERNEL VERSION DEPENDENCY
+ *************************************************************************/
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 12, 0)
+#define CURRENT_TIME_SEC	timespec_trunc(current_kernel_time(), NSEC_PER_SEC)
+#endif
+
+
 /*
  * sdfat_fs_error reports a file system problem that might indicate fa data
  * corruption/inconsistency. Depending on 'errors' mount option the
@@ -75,7 +83,7 @@ void __sdfat_fs_error(struct super_block *sb, int report, const char *fmt, ...)
 		pr_err("[SDFAT](%s[%d:%d]):ERR: %pV\n",
 			sb->s_id, MAJOR(bd_dev), MINOR(bd_dev), &vaf);
 #ifdef CONFIG_SDFAT_SUPPORT_STLOG
-		if (opts->errors == SDFAT_ERRORS_RO && !SDFAT_IS_SB_RDONLY(sb)) {
+		if (opts->errors == SDFAT_ERRORS_RO && !(sb->s_flags & MS_RDONLY)) {
 			ST_LOG("[SDFAT](%s[%d:%d]):ERR: %pV\n",
 				sb->s_id, MAJOR(bd_dev), MINOR(bd_dev), &vaf);
 		}
@@ -86,12 +94,8 @@ void __sdfat_fs_error(struct super_block *sb, int report, const char *fmt, ...)
 	if (opts->errors == SDFAT_ERRORS_PANIC) {
 		panic("[SDFAT](%s[%d:%d]): fs panic from previous error\n",
 			sb->s_id, MAJOR(bd_dev), MINOR(bd_dev));
-	} else if (opts->errors == SDFAT_ERRORS_RO && !SDFAT_IS_SB_RDONLY(sb)) {
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 14, 0)
+	} else if (opts->errors == SDFAT_ERRORS_RO && !(sb->s_flags & MS_RDONLY)) {
 		sb->s_flags |= MS_RDONLY;
-#else
-		sb->s_flags |= SB_RDONLY;
-#endif
 		sdfat_statistics_set_mnt_ro();
 		pr_err("[SDFAT](%s[%d:%d]): Filesystem has been set "
 			"read-only\n", sb->s_id, MAJOR(bd_dev), MINOR(bd_dev));
@@ -175,7 +179,7 @@ static time_t accum_days_in_year[] = {
 };
 
 /* Convert a FAT time/date pair to a UNIX date (seconds since 1 1 70). */
-void sdfat_time_fat2unix(struct sdfat_sb_info *sbi, struct timespec_compat *ts,
+void sdfat_time_fat2unix(struct sdfat_sb_info *sbi, struct timespec *ts,
 		DATE_TIME_T *tp)
 {
 	time_t year = tp->Year;
@@ -198,7 +202,7 @@ void sdfat_time_fat2unix(struct sdfat_sb_info *sbi, struct timespec_compat *ts,
 }
 
 /* Convert linear UNIX date to a FAT time/date pair. */
-void sdfat_time_unix2fat(struct sdfat_sb_info *sbi, struct timespec_compat *ts,
+void sdfat_time_unix2fat(struct sdfat_sb_info *sbi, struct timespec *ts,
 		DATE_TIME_T *tp)
 {
 	time_t second = ts->tv_sec;
@@ -262,7 +266,7 @@ void sdfat_time_unix2fat(struct sdfat_sb_info *sbi, struct timespec_compat *ts,
 
 TIMESTAMP_T *tm_now(struct sdfat_sb_info *sbi, TIMESTAMP_T *tp)
 {
-	struct timespec_compat ts = CURRENT_TIME_SEC;
+	struct timespec ts = CURRENT_TIME_SEC;
 	DATE_TIME_T dt;
 
 	sdfat_time_unix2fat(sbi, &ts, &dt);
