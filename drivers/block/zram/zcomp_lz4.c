f65157c6ba0f15d19ed24423beaad6cf79cc89e1
Adrian DC <radian.dc@gmail.com>
2020-08-22 02:05:39 +0200

    zram: change LZ4 modules to work with the new LZ4 module version
     * Apply LZ4 changes to LZ4 ZRAM against the new LZ4 API based on
       "lib/lz4: remove back-compat wrappers"
       Change-Id: I42ddab115646a6c9cec3dd601869e5dd80e4ab9b
    
     * Define dst_len = PAGE_SIZE * 2 by default upon compression
       based on the same comment from the upstream API commit
       "zram: switch to crypto compress API"
       Change-Id: Id8aa40ef244651e2702975572e8c3fa407faf36b
    
    Change-Id: Ibc0eec49af84c02e24509c945609bef86ef48bad
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- drivers/block/zram/zcomp_lz4.c
+++ drivers/block/zram/zcomp_lz4.c
@@ -35,16 +35,40 @@ static void zcomp_lz4_destroy(void *private)
 static int zcomp_lz4_compress(const unsigned char *src, unsigned char *dst,
 		size_t *dst_len, void *private)
 {
-	/* return  : Success if return 0 */
-	return lz4_compress(src, PAGE_SIZE, dst, dst_len, private);
+	/*
+	 * Our dst memory (zstrm->buffer) is always `2 * PAGE_SIZE' sized
+	 * because sometimes we can endup having a bigger compressed data
+	 * due to various reasons: for example compression algorithms tend
+	 * to add some padding to the compressed buffer. Speaking of padding,
+	 * comp algorithm `842' pads the compressed length to multiple of 8
+	 * and returns -ENOSP when the dst memory is not big enough, which
+	 * is not something that ZRAM wants to see. We can handle the
+	 * `compressed_size > PAGE_SIZE' case easily in ZRAM, but when we
+	 * receive -ERRNO from the compressing backend we can't help it
+	 * anymore. To make `842' happy we need to tell the exact size of
+	 * the dst buffer, zram_drv will take care of the fact that
+	 * compressed buffer is too big.
+	 */
+	*dst_len = PAGE_SIZE * 2;
+
+	*dst_len = LZ4_compress_default(src, dst, PAGE_SIZE, *dst_len, private);
+
+	if (!*dst_len)
+		return -1;
+
+	return 0;
 }
 
 static int zcomp_lz4_decompress(const unsigned char *src, size_t src_len,
 		unsigned char *dst)
 {
 	size_t dst_len = PAGE_SIZE;
-	/* return  : Success if return 0 */
-	return lz4_decompress_unknownoutputsize(src, src_len, dst, &dst_len);
+	dst_len = LZ4_decompress_safe(src, dst, src_len, dst_len);
+
+	if (dst_len > 0)
+		return 0;
+
+	return -1;
 }
 
 struct zcomp_backend zcomp_lz4 = {
