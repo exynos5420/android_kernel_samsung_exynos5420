ee2ce3428123652d268953f92f7fd0ead6452da1
Shev T <evgeny.shishkov@gmail.com>
2021-08-05 19:12:47 +0200

    SCFS: Remove scfs
    This filesystem (Samsung/Stackable Compression Filesystem) won't be
    compilable after the O_TMPFILE chain, and given that we don't use it
    for anything.
    
    This commit is imported from https://gitlab.com/ShevT/kernel_samsung_lt033g/-/commit/1feee133b72862d6f3ce4d6439a63c532247a590
    The authorship belongs to ShEV.
    
    Change-Id: I737beb94d5e1d7c9804876f8533e9f98efdcb367

--- include/linux/page-flags.h
+++ include/linux/page-flags.h
@@ -108,10 +108,6 @@ enum pageflags {
 #ifdef CONFIG_TRANSPARENT_HUGEPAGE
 	PG_compound_lock,
 #endif
-#ifdef CONFIG_SCFS_LOWER_PAGECACHE_INVALIDATION
-	PG_scfslower,
-	PG_nocache,
-#endif
 #ifdef CONFIG_ZCACHE
 	PG_was_active,
 #endif
@@ -284,11 +280,6 @@ TESTSCFLAG(HWPoison, hwpoison)
 #else
 PAGEFLAG_FALSE(HWPoison)
 #define __PG_HWPOISON 0
-#endif
-
-#ifdef CONFIG_SCFS_LOWER_PAGECACHE_INVALIDATION
-PAGEFLAG(Scfslower, scfslower)
-PAGEFLAG(Nocache, nocache)
 #endif
 
 u64 stable_page_flags(struct page *page);
